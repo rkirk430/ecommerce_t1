c21979443007399edc86452f010de7b6a56d84f3
rkirk430 <r.kirk430@gmail.com>
2022-07-14 15:06:52 -0400

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Home from './pages/Home';
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <Home />
+    </>
+  )
+}
+
+export default App
+
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './navbar.css'
+
+
+
+const Navbar = () => {
+  return (
+    <div className='nav__container'>Navbar</div>
+  )
+}
+
+export default Navbar
