3b821fa79be2111e75b048d8a3fdd4aef9ca1ba5
rkirk430 <r.kirk430@gmail.com>
2022-07-17 16:32:51 -0400

    Categories

---
+++ src/components/CategoryItem.jsx
@@ -0,0 +1,20 @@
+import { ListItem } from '@mui/material'
+import React from 'react'
+
+const CategoryItem = ({item}) => {
+  return (
+    <div className="categoryitem__container">
+        <img src={item.img} className="categoryitem__container-image" />
+        <div className="categoryitem__container-info">
+            <div className="categoryitem__container-infotitle">
+                {item.title}
+            </div>
+            <button className='categoryitem__container-infobutton'>Shop Now</button>
+        </div>
+    </div>  
+
+
+  )
+}
+
+export default CategoryItem
