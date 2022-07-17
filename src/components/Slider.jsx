d6e261015cf35ae0470f072606c5dcf8b053e59e
rkirk430 <r.kirk430@gmail.com>
2022-07-17 16:17:47 -0400

    Skipping Slider for Now

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,36 +1,36 @@
-import React from 'react'
-import './slider.css'
-import { useState } from "react";
-import { sliderItems } from "../data";
-import { mobile } from "../responsive"
-import { ArrowLeftOutlined } from '@mui/icons-material';
+// import React from 'react'
+// import './slider.css'
+// import { useState } from "react";
+// import { sliderItems } from "../data";
+// import { mobile } from "../responsive"
+// import { ArrowLeftOutlined } from '@mui/icons-material';
 
 
 
 
-const Slider = () => {
-    const [slideIndex, setSlideIndex] = useState(0);
-    const handleClick = (direction) => {
-        if (direction === "left") {
-            setSlideIndex(slideIndex > 0 ? slideIndex -1 : 2);
-        } else {
-            setSlideIndex(slideIndex < 2 ? slideIndex +1 : 0);
-        }
-    };
+// const Slider = () => {
+//     const [slideIndex, setSlideIndex] = useState(0);
+//     const handleClick = (direction) => {
+//         if (direction === "left") {
+//             setSlideIndex(slideIndex > 0 ? slideIndex -1 : 2);
+//         } else {
+//             setSlideIndex(slideIndex < 2 ? slideIndex +1 : 0);
+//         }
+//     };
 
-  return (
-    <div className="slidercontainer">
-        <div className="slidercontainer-arrow" direction="left" onClick={() => handleClick("left")}>
-            <ArrowLeftOutlined />
-        </div>
+//   return (
+//     <div className="slidercontainer">
+//         <div className="slidercontainer-arrow" direction="left" onClick={() => handleClick("left")}>
+//             <ArrowLeftOutlined />
+//         </div>
 
-        <div className="wrapper" slideIndex={slideIndex}>
-            {sliderItems.map((item) => (
+//         <div className="wrapper" slideIndex={slideIndex}>
+//             {sliderItems.map((item) => (
                 
-            ))}
-        </div>
-    </div>
-  )
-}
+//             ))}
+//         </div>
+//     </div>
+//   )
+// }
 
-export default Slider
+// export default Slider
