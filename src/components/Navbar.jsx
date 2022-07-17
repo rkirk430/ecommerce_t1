4b292bca15dc3ed454965f252024d3284c9b6ef9
rkirk430 <r.kirk430@gmail.com>
2022-07-17 13:25:50 -0400

    Styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import './navbar.css'
+import SearchIcon from '@mui/icons-material/Search';
 
 
 
 const Navbar = () => {
   return (
-    <div className='nav__container'>Navbar</div>
+    <div className='nav__container'>
+      <div className="nav__container-wrapper">
+        <div className="nav__left">
+          <div className="nav__left-language">
+            EN
+          </div>
+          <div className="nav__left-search">
+            <SearchIcon />
+          </div>
+        </div>
+        <div className="nav__center">
+          Center
+        </div>
+        <div className="nav__right">
+          Right
+        </div>
+      </div>
+    </div>
   )
 }
 
