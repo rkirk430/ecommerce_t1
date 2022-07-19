78b2a0aa2bba4d6e48355431afdfef12115bb777
rkirk430 <r.kirk430@gmail.com>
2022-07-19 15:32:59 -0400

    Slider Start

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import Navbar from '../components/Navbar'
 import Newsletter from '../components/Newsletter'
 import Products from '../components/Products'
 import Footer from '../components/Footer'
+import Slider from '../components/Slider'
 
 
 const Home = () => {
@@ -12,6 +13,7 @@ const Home = () => {
     <>
         <Announcement />
         <Navbar />
+        <Slider />
         <Categories />
         <Products />
         <Newsletter />
