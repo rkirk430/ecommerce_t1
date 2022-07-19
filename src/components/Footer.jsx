6ef8b39306de0619a4e94822c78570c728067a19
rkirk430 <r.kirk430@gmail.com>
2022-07-19 14:05:18 -0400

    Footer Center Styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,18 +11,25 @@ const Footer = () => {
             </h1>
 
             <div className="footer__left-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam enim iusto nulla rem molestias laudantium, architecto ipsam illo. Nobis officiis iusto alias esse vel provident nisi obcaecati non exercitationem aperiam!</div>
-
             <div className="footer__left-socials">
                 <div className="footer__left-socialfb"><Facebook/></div>
                 <div className="footer__left-socialig"><Instagram/></div>
                 <div className="footer__left-socialtwt"><Twitter/></div>
                 <div className="footer__left-socialpins"><Pinterest/></div>
             </div>
-
         </div>
 
         <div className="footer__center">
-
+            <h3 className="footer__center">Useful Links</h3>
+            <ul className="footer__center-ul">
+                <li className="footer__center-li">Home</li>
+                <li className="footer__center-li">Cart</li>
+                <li className="footer__center-li">Men's Fashion</li>
+                <li className="footer__center-li">Women's Fashion</li>
+                <li className="footer__center-li">Accessories</li>
+                <li className="footer__center-li">My Account</li>
+                <li className="footer__center-li">Shipping Information</li>
+            </ul>
         </div>
 
         <div className="footer__right">
