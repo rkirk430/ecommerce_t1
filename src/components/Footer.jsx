19fb5dd60eacb6531b064b93c783594b6cc04325
rkirk430 <r.kirk430@gmail.com>
2022-07-19 13:52:45 -0400

    Footer Left Styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,22 @@
+import { Facebook, Instagram, Pinterest, Twitter } from '@mui/icons-material'
 import React from 'react'
+import "./footer.css"
 
 const Footer = () => {
   return (
     <div className="footer__container">
         <div className="footer__left">
-            <div className="footer__left-logo">
+            <h1 className="footer__left-logo">
                 LAMA.
-            </div>
+            </h1>
 
             <div className="footer__left-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam enim iusto nulla rem molestias laudantium, architecto ipsam illo. Nobis officiis iusto alias esse vel provident nisi obcaecati non exercitationem aperiam!</div>
 
             <div className="footer__left-socials">
-                <div className="footer__left-social"></div>
+                <div className="footer__left-socialfb"><Facebook/></div>
+                <div className="footer__left-socialig"><Instagram/></div>
+                <div className="footer__left-socialtwt"><Twitter/></div>
+                <div className="footer__left-socialpins"><Pinterest/></div>
             </div>
 
         </div>
