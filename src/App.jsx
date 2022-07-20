6a09867eb7f6531b510770038b9f6bfe60d6a8d2
rkirk430 <r.kirk430@gmail.com>
2022-07-20 16:32:54 -0400

    Product List HTML

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import './App.css';
 import Home from './pages/Home';
 
 import React from 'react'
+import ProductList from './pages/ProductList';
 
 const App = () => {
   return (
     <>
-    <Home />
+    {/* <Home /> */}
+    <ProductList />
     </>
   )
 }
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -2,7 +2,48 @@ import React from 'react'
 
 const ProductList = () => {
   return (
-    <div>ProductList</div>
+    <div className="productlist__container">
+      <h1 className="productlist-title"> Dresses</h1>
+      <div className="productlist__filtercontainer">
+
+        {/* Filter Color & Size  */}
+        <div className="productlist__filter">
+          <span className="productlist__filtertext">
+            Filter Products:
+          </span>
+          <select name="" id="" className="productlist__select">
+            <option disabled selected>Color</option>
+            <option>White</option>
+            <option>Black</option>
+            <option>Red</option>
+            <option>Blue</option>
+            <option>Yellow</option>
+          </select>
+          <select name="" id="" className="productlist__select">
+            <option disabled selected> Size </option>
+            <option> XS </option>
+            <option> S </option>
+            <option> M </option>
+            <option> L </option>
+          </select>
+        </div>
+
+        {/* Filter Product Sorting */}
+        <div className="productlist__filter">
+          <span className="productlist__filtertext">
+            Sort Products
+          </span>
+          <select name="" id="" className="productlist__select">
+            <option selected>Newest</option>
+            <option>Price (asc)</option>
+            <option>Price (desc)</option>
+          </select>
+        </div>
+
+
+
+      </div>
+    </div>
   )
 }
 
